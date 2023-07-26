ea66523a79765db7c0ff39b1a104780074e22c59
Vitalii Dmytriv <vitaliqq.dmutriv@gmail.com>
2023-07-26 08:20:04 +0300

    Add configuration to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 function App() {
-    return <div>
-        <h1>Bladasdas</h1>
-        <h2>Second commit</h2>
-        <h3>Second commit</h3>
-    </div>
+    return (
+        <div>
+            <div>
+                <h4>dasdas</h4>
+            </div>
+            <h1>Bladasdas</h1>
+            <h2>Second commit</h2>
+            <h3>Second commit</h3>
+        </div>
+    );
 }
 
 export default App;
